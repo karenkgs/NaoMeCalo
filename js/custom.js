4dedeaee9dfcb37cad83343fc4b013ea794b39b8
Karen <karenkgs@gmail.com>
2015-03-01 14:37:41 -0300

    Mudanças de layout

---
+++ js/custom.js
@@ -0,0 +1,38 @@
+$(window).scroll(function() {
+  var navbarColor = "255,255,255";//color attr for rgba
+  var smallLogoHeight = $('.small-logo').height();
+  var bigLogoHeight = $('.big-logo').height();
+  var navbarHeight = $('.navbar').height(); 
+  
+  var smallLogoEndPos = 0;
+  var smallSpeed = (smallLogoHeight / bigLogoHeight);
+  
+  var ySmall = ($(window).scrollTop() * smallSpeed); 
+  
+  var smallPadding = navbarHeight - ySmall;
+  if (smallPadding > navbarHeight) { smallPadding = navbarHeight; } 
+  if (smallPadding < smallLogoEndPos) { smallPadding = smallLogoEndPos; }
+  if (smallPadding < 0) {
+	  smallPadding = 0;
+	  $('.navbar').css({"background-color": 'rgb('+navbarColor+')'});
+  }
+  
+   $('.small-logo-container ').css({ "padding-top": smallPadding}); 
+  
+  /* var navOpacity = ySmall / smallLogoHeight; 
+  if  (navOpacity > 1) { navOpacity = 1; }
+  if (navOpacity < 0 ) { navOpacity = 0; }
+  var navBackColor = 'rgba(' + navbarColor + ',' + navOpacity + ')';
+	$('.navbar').css({"background-color": navBackColor});  */
+   
+  
+   var shadowOpacity = navOpacity * 0.4;
+  if ( ySmall > 1) {
+    $('.navbar').css({"box-shadow": "0 2px 3px rgba(0,0,0," + shadowOpacity + ")"});
+  } else {
+    $('.navbar').css({"box-shadow": "none"});
+  } 
+  
+  
+  
+});
