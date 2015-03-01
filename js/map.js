8deba0ca2876385f5bea05f755fcf06b403bd696
apdaros <anapauladaros@gmail.com>
2015-03-01 12:24:01 -0300

    Ajustando configurações do scroll do mapa

--- js/map.js
+++ js/map.js
@@ -5,6 +5,7 @@ function initialize() {
   var  mapOptions = {
 	  center: new google.maps.LatLng(-30.03, -51.21),
 	  zoom: 14,
+    scrollwheel: false,
 	  };
 
   var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
