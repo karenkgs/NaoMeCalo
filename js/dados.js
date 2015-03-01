e6b348d3fed2d8b37b2b780a92887b529463f81e
ManoelaR <manoela.g.rivera@gmail.com>
2015-03-01 15:22:41 -0300

    json dicas

---
+++ js/dados.js
@@ -0,0 +1,69 @@
+var dados =
+{"Grande Porto Alegre" :
+	{
+		"Porto Alegre": [
+			{
+				"nome":"Delegacia Especializada de Atendimento a Mulher (DEAM) - Porto Alegre",
+				"endereco":"Avenida João Pessoa, nº 2050 - Bairro: Azenha - Referência: No Palácio da Polícia",
+				"telefone":"(51)3288-2172",
+				"email":" poa-dm@pc.rs.gov.br"
+			},
+			{
+				"nome":"Centro de Referência da Mulher Vânia Araújo Machado - Porto Alegre",
+				"endereco":"Rua Tuiuti, nº 10 - Bairro: Centro - Referência: Esquina com Desembargador André da Rocha",
+				"telefone":"08005410803",
+				"email":"crm@spm.rs.gov.br"
+			},
+			{
+				"nome":"Centro de Referência de Atendimento á Mulher Municipal - Márcia Calixto - Porto Alegre",
+				"endereco":"Rua Siqueira Campos, nº 1184, 16º andar - Bairro: Centro Histórico",
+				"telefone":"(51)3289-5102",
+				"email":"cram@smdh.prefpoa.com.br"
+			},
+			{
+				"nome":"2º Juizado da Violência Doméstica e Familiar - Porto Alegre",
+				"endereco":"Rua Márcio Veras Vidor, nº 10, sala 518 - Bairro: Centro",
+				"telefone":"(51)3210-6651",
+				"email":"srposent2jzvdfamm@tj.rs.gov.br"
+			},
+			{
+				"nome":"1º Juizado da Violência Doméstica e Familiar - Porto Alegre",
+				"endereco":"Rua Márcio Veras Vidor, nº 10 , Fórum Central, 5º Andar, Sala 501 - Bairro: Praia de Belas",
+				"telefone":"(51)3210-6670",
+				"site":"http://www.tj.rs.jus.br"
+			},
+			{
+				"nome":"Núcleo de Família e de Defesa da Mulher Vítima de Violência - Porto Alegre",
+				"endereco":"Rua Sete de Setembro, n° 666 - Térreo e 7º Andar - Bairro: Centro",
+				"telefone":"(51)3211-2233",
+				"email":"nudem@dpe.rs.gov.br"
+			},
+			{
+				"nome":"Núcleo de Combate a Violência Doméstica (NUCLEVID) - Porto Alegre",
+				"endereco":"Avenida Aureliano de Figueiredo Pinto, nº 80,10º Andar, Torre Norte - Bairro: Praia de Belas",
+				"telefone":"(51)3295-1167",
+				"email":"caocrim@mp.rs.gov.br"
+			},
+			{
+				"nome":"Hospital Nossa Senhora Conceição - Porto Alegre",
+				"endereco":"Avenida Francisco Trein, nº 596 - Bairro Cristo Redentor",
+				"telefone":"(51)3357-2161",
+				"email":"alisete@ghc.com.br"
+			},
+			{
+				"nome":"Hospital Pronto Socorro de Porto Alegre",
+				"endereco":"Endereço:Largo Teodoro Herzl, s/nº - Bairro Bom Fim",
+				"telefone":"(51)3289-7999"
+			},
+			{
+				"nome":"Hospital Fêmina - Porto Alegre",
+				"endereco":"Rua Mostardeiro, nº 17 - Bairro: Moinhos do Vento",
+				"telefone":"(51)3314-5200",
+				"email":"hfe@ghc.com.br"
+			},
+			{
+				"nome":"Casa de Apoio Viva Maria - Porto Alegre"
+			}
+		]
+	}
+}
